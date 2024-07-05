ccabe74ca89df7304d67b5a52a7943c257f39fc3
theredgirl <72979321+theredgirl@users.noreply.github.com>
2024-07-04 21:41:47 -0400

    changes that add functionality to project
    adding iterating through sheets (not working atm)

---
+++ normalizetest.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+allData = pd.read_excel("070124_m4_raw.xlsx",sheet_name=None)
+
+for name in allData():
+    wellData = name
+    well = np.array(wellData)
+    time = ((well[:,0])*10)-10
+    cells = well[:,1:-1]
+    background = well[:,-1]
+
+    #subtract background from cells
+    zeroCells = cells - background.reshape(-1,1)
+    #average the first 3 frames together to create a baseline which will be subtracted later
+    blCells = np.mean(zeroCells[0:3],axis = 0)
+    #normalize cells
+    normCells = (zeroCells - blCells)/blCells
+    #take average of all cells for the well
+    wellAvg = np.mean(normCells[:],axis = 1)
+    
+    wellAvgPrint = pd.DataFrame(wellAvg, time)
+    print(wellAvgPrint)
